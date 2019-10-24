0acd321e5d560e69dfc90578a5aaf30c608efe32
simar pannu <simarpannu@gmail.com>
2019-10-24 15:35:14 -0500

    added 00_csharp

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using sc = System.Console; //import with alias
+
+namespace HelloWorld.Client
+{
+
+  /// <summary>
+  /// starts the program
+  /// </summary>
+
+
+  internal class Program
+  {
+
+
+    private static void Main(string[] args)
+    {
+      sc.WriteLine("Hello World!");
+
+
+    }
+  }
+}
