37340136f8bc4dd141ac3708873e16cc633db9e6
simar pannu <simarpannu@gmail.com>
2019-12-10 10:03:57 -0600

    added testing

---
+++ 3_devops/Devops.Test/ClientTest.cs
@@ -0,0 +1,30 @@
+using Devops.Client.Controllers;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Devops.Test
+{
+    public class ClientTest{
+     
+     private readonly ILogger<HomeController> logger = LoggerFactory.Create(o=>o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<HomeController>();
+      [Fact]
+      public void Test_IndexPage(){
+        //arrange
+        var home = new HomeController(logger);
+
+        //act
+        var index = home.Index();
+       
+
+        //assert
+        Assert.NotNull(index);
+        
+      }
+            [Fact]
+      public void Test_PrivacyPage(){
+var home = new HomeController(logger);
+ var privacy = home.Privacy();
+ Assert.NotNull(privacy);
+      }
+    }
+}
